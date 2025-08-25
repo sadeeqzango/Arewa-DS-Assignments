247f4dc320e8da2309475b4d8d855e4a6453493b
sadeeqzango <40058134+sadeeqzango@users.noreply.github.com>
2025-08-25 11:40:40 +0100

    Add files via upload

---
+++ variable.py
@@ -0,0 +1,52 @@
+# 'Day 2: 30 Days of python programming'
+First_name = "Abubakar"
+Last_name  = "Hamisu"
+Full_name  = "Zango"
+Country    = "Nigeria"
+City       = "Kano"
+Age        = 30
+Year       = "2025"
+is_Married = True
+is_True    = True
+is_Light_on = True
+Name, Age   = "Sadik Zango", 30
+print(type(First_name))
+print(type(Last_name))
+print(type(Full_name))
+print(type(Country))
+print(type(City))
+print(type(Age))
+print(type(Year))
+print(type(is_Married))
+print(type(is_True))
+print(type(is_Light_on))
+print(type(Name))
+print(type(Age))
+
+# excercise 2
+
+print(len(First_name))
+len(First_name) == len(Last_name)
+num_one = 5
+num_two = 4
+total = num_one + num_two
+diff  = num_one - num_two
+product = num_one * num_two
+division = num_one / num_two
+remainder = num_two % num_one
+exp = num_two ** num_one
+floor_division = num_one // num_two
+Radius = 30
+pi = 3.142
+Area_of_circle = pi * Radius**2
+circum_of_circle = 2 * pi * Radius
+print(total)
+print(diff)
+print(product)
+print(division)
+print(remainder)
+print(exp)
+print(floor_division)
+print(Radius)
+print(Area_of_circle)
+print(circum_of_circle)
